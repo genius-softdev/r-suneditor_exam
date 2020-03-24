a4e53619c21073ba0c33b92eb265100f7bd6bc1b
unknown <webdevelopercrane@gmail.com>
2020-03-24 14:07:02 +0700

    Implement SunEditor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SunEditor from "suneditor-react";
+import "suneditor/dist/css/suneditor.min.css";
 
 function App() {
+  const handleChange = content => {
+    console.log(content);
+  };
+  const handleBlur = e => {
+    console.log(e);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>My Other Contents</p>
+      <SunEditor
+        setOptions={{
+          buttonList: [["bold", "italic", "underline"], ["list"], ["link"]],
+          width: "500px",
+          height: "200px",
+          mode: "classic",
+          resizingBar: false
+        }}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
     </div>
   );
 }
